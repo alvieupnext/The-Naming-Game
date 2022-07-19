5031035102df3c02f870d5a7f950a42c8ac7ea78
Alvaro Vargas Guerrero <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-07-19 15:39:17 +0200

    Added equalityCreation test

--- tests/MatrixFactoryTest.py
+++ tests/MatrixFactoryTest.py
@@ -24,9 +24,18 @@ class MatrixFactoryTest(unittest.TestCase):
     self.assertTrue(np.allclose(scaleFree, np.tril(scaleFree)))  # check if lower triangular
     self.assertTrue(np.allclose(smallWorld, np.tril(smallWorld)))  # check if lower triangular
 
+  def test_equalityCreation(self):
+    """A lattice matrix created from a symmetrical allowance should be the same as the lower triangular of a matrix created without this allowance"""
+    #Full matrix
+    l1 = self.aFactory.makeLatticeMatrix(20, 4)
+    #Lower triangular matrix
+    l2 = self.sFactory.makeLatticeMatrix(20, 4)
+    self.assertTrue(np.allclose(l2, np.tril(l1)))  # check if same
+
   def test_latticeConnections(self):
     """Each agent in a lattice is connected exactly to a certain amount of neighbors"""
     def doTest(pair):
+      print("Testing Pair: " + str(pair))
       agents = pair[0]
       neighbours = pair[1]
       asymLattice = self.aFactory.makeLatticeMatrix(agents, neighbours)
@@ -34,7 +43,6 @@ class MatrixFactoryTest(unittest.TestCase):
       nonZeroRowNrs, nonZeroColumnNrs = np.nonzero(asymLattice)
       nonZeroCount = len(nonZeroRowNrs)
       # An asymmetrical lattice matrix has 20 agent *4 neighbors= 80 connections
-      print(asymLattice)
       self.assertEqual(agents*neighbours, nonZeroCount, "Asymmetrical Error")
       nonZeroRowNrsSym, nonZeroColumnNrsSym = np.nonzero(symLattice)
       nonZeroCountSym = len(nonZeroRowNrsSym)
@@ -47,16 +55,27 @@ class MatrixFactoryTest(unittest.TestCase):
       doTest(pair)
 
   def test_scaleFreeConnections(self):
-    """A matrix factory should be able to produce a scale free matrix"""
-    scaleFree = self.aFactory.makeScaleFreeMatrix(self.agents)
-    #
-    nonZeroRowNrs, nonZeroColumnNrs = np.nonzero(scaleFree)
-    nonZeroCount = len(nonZeroRowNrs)
-    #expected non zero elements = 8 (starting connections from 4 agents) + 2 (two-way connection) * Established Links (default 2) * 16 (agents - 4) = 72
-    self.assertEqual(72, nonZeroCount)
+    """Each agent in a scale free matrix is connected depending on the establishing links"""
 
-  def test_scaleFreeLinks(self):
-    """A scale free matrix should have a certain amount of connections depending """
+    def doTest(pair):
+      print("Testing Pair: " + str(pair))
+      agents = pair[0]
+      establishedLinks = pair[1]
+      asymScaleFree= self.aFactory.makeScaleFreeMatrix(agents, establishedLinks)
+      symScaleFree = self.sFactory.makeScaleFreeMatrix(agents)
+      nonZeroRowNrs, nonZeroColumnNrs = np.nonzero(asymScaleFree)
+      nonZeroCount = len(nonZeroRowNrs)
+      #expected non zero elements = 8 (starting connections from 4 agents) + 2 (two-way connection) * Established Links * remaining agents (agents - 4)
+      self.assertEqual(8 + 2 * establishedLinks * (agents - 4), nonZeroCount, "Asymmetrical Error")
+      nonZeroRowNrsSym, nonZeroColumnNrsSym = np.nonzero(symScaleFree)
+      nonZeroCountSym = len(nonZeroRowNrsSym)
+      # The symmetrical lattice should be equal to half the asymmetrical connections
+      self.assertEqual(4 + establishedLinks * (agents - 4), nonZeroCountSym, "Symmetrical Error")
+
+    scaleFreePairs = [(20, 2), (37, 6), (14, 3), (87, 7)]
+
+    for pair in scaleFreePairs:
+      doTest(pair)
 
 
 
