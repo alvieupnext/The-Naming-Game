3a511b780013ad85d0082509b607cafbc43fdcab
Alvaro Vargas Guerrero <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-07-19 13:51:32 +0200

    Finished asymCreation and symCreation tests

--- tests/MatrixFactoryTest.py
+++ tests/MatrixFactoryTest.py
@@ -2,38 +2,58 @@ import unittest
 import MatrixFactory as mf
 import numpy as np
 
-class AsymmetricalTest(unittest.TestCase):
+class MatrixFactoryTest(unittest.TestCase):
   #setup the test
   def setUp(self):
     #create a new asymmetrical matrix factory
-    self.factory = mf.MatrixFactory(sym=False)
+    self.aFactory = mf.MatrixFactory(sym=False)
+    self.sFactory = mf.MatrixFactory(sym=True)
     self.agents = 20
     self.neighbours = 4
     self.randomLinks = 5
 
-  def test_lattice(self):
-    """A matrix factory should be able to produce a lattice matrix"""
-    lattice = self.factory.makeLatticeMatrix(self.agents, self.neighbours)
-    #An asymmetrical lattice matrix has 20*4= 80 connections
-    nonZeroRowNrs, nonZeroColumnNrs = np.nonzero(lattice)
-    nonZeroCount = len(nonZeroRowNrs)
-    self.assertEqual(80, nonZeroCount)
+  def test_asymCreation(self):
+    """A matrix factory should be able to produce all implemented matrices"""
+    lattice = self.aFactory.makeLatticeMatrix(self.agents, self.neighbours)
+    scaleFree = self.aFactory.makeScaleFreeMatrix(self.agents)
+    smallWorld = self.aFactory.makeSmallWorldMatrix(self.agents, self.neighbours, self.randomLinks)
 
-  def test_scaleFree(self):
+  def test_symCreation(self):
+    """A matrix factory should be able to produce a lower triangular matrix version of every matrix"""
+    lattice = self.sFactory.makeLatticeMatrix(self.agents, self.neighbours)
+    scaleFree = self.sFactory.makeScaleFreeMatrix(self.agents)
+    smallWorld = self.sFactory.makeSmallWorldMatrix(self.agents, self.neighbours, self.randomLinks)
+    self.assertTrue(np.allclose(lattice, np.tril(lattice)))  # check if lower triangular
+    self.assertTrue(np.allclose(scaleFree, np.tril(scaleFree)))  # check if lower triangular
+    self.assertTrue(np.allclose(smallWorld, np.tril(smallWorld)))  # check if lower triangular
+
+  def test_latticeConnections(self):
+    """Each agent in a lattice is connected exactly to a certain amount of neighbors"""
+    asymLattice = self.aFactory.makeLatticeMatrix(self.agents, self.neighbours)
+    symLattice = self.sFactory.makeLatticeMatrix(self.agents, self.neighbours)
+    nonZeroRowNrs, nonZeroColumnNrs = np.nonzero(asymLattice)
+    nonZeroCount = len(nonZeroRowNrs)
+    # An asymmetrical lattice matrix has 20 agent *4 neighbors= 80 connections
+    self.assertEqual(80, nonZeroCount)
+    nonZeroRowNrsSym, nonZeroColumnNrsSym = np.nonzero(symLattice)
+    nonZeroCountSym = len(nonZeroRowNrsSym)
+    # The symmetrical lattice should be equal to half the asymmetrical connections (in this case 40)
+    self.assertEqual(nonZeroCount / 2, nonZeroCountSym)
+
+  def test_scaleFreeCreation(self):
     """A matrix factory should be able to produce a scale free matrix"""
-    scaleFree = self.factory.makeScaleFreeMatrix(self.agents)
+    scaleFree = self.aFactory.makeScaleFreeMatrix(self.agents)
     #
     nonZeroRowNrs, nonZeroColumnNrs = np.nonzero(scaleFree)
     nonZeroCount = len(nonZeroRowNrs)
-    self.assertEqual(80, nonZeroCount)
+    #expected non zero elements = 8 (starting connections from 4 agents) + 2 (two-way connection) * Established Links (default 2) * 16 (agents - 4) = 72
+    self.assertEqual(72, nonZeroCount)
+
+  def test_scaleFreeLinks(self):
+    """A scale free matrix should have a certain amount of connections depending """
+
 
 
 if __name__ == '__main__':
   unittest.main()
-
-# MatrixFactorySuite = unittest.TestSuite()
-# MatrixFactorySuite.addTest(AsymmetricalTest())
-# #create test runner
-# runner = unittest.TextTestRunner()
-# runner.run(MatrixFactorySuite)
 
