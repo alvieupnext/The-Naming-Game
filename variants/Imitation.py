ee9269efa0bde73f02f9c18b751ab6b09b81a5fd
Alvaro Vargas Guerrero <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-07-22 16:17:53 +0200

    Added a second version of Imitation according to Guy's requirements

--- variants/Imitation.py
+++ variants/Imitation.py
@@ -1,5 +1,6 @@
 from variants.BaselineNG import *
 #according to the paper document (The Naming Game)
+#remove procedure could be moved into its own class (if desired)
 class Imitation(BaselineNG):
   #Code for removing a topic from Agent memory
   def remove(self, topic, agent):
@@ -17,11 +18,17 @@ class Imitation(BaselineNG):
 
 #according to Guy Nagels, only success should clear memory
 class Imitationv2(BaselineNG):
+  #Code for removing a topic from Agent memory, preserving the original binding
+  def remove(self, topic, agent, name):
+    for pairName, pairTopic in self.memory[agent]:
+      #if name of pair corresponds with the adopted name
+      if pairTopic == topic and name != pairName:
+        self.memory[agent].remove((pairName, topic))
+        self.display(f"Removed this pair from Agent memory {agent}: {pairName}, {pairTopic}")
   #same code but only performs clear on success
   def success(self, speaker, listener, topic, name):
     #remove all previous bindings of the topic from both speaker and listener memory
-    self.remove(topic, speaker)
-    self.remove(topic, listener)
-    #adopt new binding for speaker and listener
-    self.adopt(name, topic, speaker)
-    self.adopt(name, topic, listener)
+    self.remove(topic, speaker, name)
+    self.remove(topic, listener, name)
+    #invoke super
+    super().success(speaker, listener, topic, name)
