aa2c261ba6ce847d5439fc50ef29699a9d3940d9
Alvaro Vargas Guerrero <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-07-22 16:07:24 +0200

    Added new remove procedure to Imitation which removes a topic from memory

--- variants/Imitation.py
+++ variants/Imitation.py
@@ -1,14 +1,16 @@
 from variants.BaselineNG import *
 #according to the paper document (The Naming Game)
 class Imitation(BaselineNG):
+  #Code for removing a topic from Agent memory
+  def remove(self, topic, agent):
+    for pairName, pairTopic in self.memory[agent]:
+      #if name of pair corresponds with the adopted name
+      if pairTopic == topic:
+        self.memory[agent].remove((pairName, topic))
+        self.display(f"Removed this pair from Agent memory {agent}: {pairName}, {pairTopic}")
+
   def adopt(self, name, topic, agent):
     #remove all previous bindings of the topic with the name
-    for pairName, topic in self.memory[agent]:
-      #if name of pair corresponds with the adopted name
-      if pairName == name:
-        self.memory[agent].remove((pairName, topic))
-        self.display(f"Removed this pair from memory: {pairName}, {topic}")
-    #add new binding
-    self.memory[agent].append((name, topic))
+    self.remove(topic, agent)
     # if succesfull, run super
     super().adopt(name, topic, agent)
