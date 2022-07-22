073dc8cf4919ba6a720d722fa5566561d68193eb
Alvaro Vargas Guerrero <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-07-22 16:10:01 +0200

    Added a second version of Imitation according to Guy's requirements

--- variants/Imitation.py
+++ variants/Imitation.py
@@ -14,3 +14,14 @@ class Imitation(BaselineNG):
     self.remove(topic, agent)
     # if succesfull, run super
     super().adopt(name, topic, agent)
+
+#according to Guy Nagels, only success should clear memory
+class Imitationv2(BaselineNG):
+  #same code but only performs clear on success
+  def success(self, speaker, listener, topic, name):
+    #remove all previous bindings of the topic from both speaker and listener memory
+    self.remove(topic, speaker)
+    self.remove(topic, listener)
+    #adopt new binding for speaker and listener
+    self.adopt(name, topic, speaker)
+    self.adopt(name, topic, listener)
