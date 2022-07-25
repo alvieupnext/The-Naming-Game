98b3d1f96f623b8f1dd20c0d35bce2b40f6da9ef
Alvaro Vargas Guerrero <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-07-25 17:52:18 +0200

    Added preferredAction graph visualizer

---
+++ graph/preferredAction.py
@@ -0,0 +1,39 @@
+from matplotlib import pyplot as plt
+from variants.BaselineNG import *
+from variants.Imitation import *
+import MatrixFactory as mf
+from variants.ABNG import *
+import Strategy
+def preferredAction(ng, actionMatrix):
+
+  fig, ax = plt.subplots()
+  numberMatrix = np.zeros(actionMatrix.shape)
+  for x in range(actionMatrix.shape[0]):
+    for y in range(actionMatrix.shape[1]):
+      if len(actionMatrix[x,y]) == 0 :
+        #indecisive
+        numberMatrix[x,y] = 0
+      elif len(actionMatrix[x,y]) == 2:
+        numberMatrix[x,y] = 3
+      elif actionMatrix[x,y][0] == 'A':
+        numberMatrix[x, y] = 1
+      else: numberMatrix[x, y] = 2
+  ax.set_title(f"Preferred Action per Agent per Iteration ({ng.strategy.__name__})")
+  ax.set_ylabel("Games played per agent")
+  ax.set_xlabel("Agents")
+  ax.matshow(numberMatrix)
+
+  plt.show()
+
+factory = mf.MatrixFactory(triangular=True)
+
+lattice = factory.makeLatticeMatrix(100, 5)
+
+ab = ABNG(iterations=100, strategy=Strategy.multi, output=["preferredAction"])
+
+output = ab.start(lattice)["preferredAction"][1]
+
+preferredAction(ab, output)
+
+
+
