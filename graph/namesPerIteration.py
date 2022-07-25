d7309632184e3d5e0d0fe8b9bbe022b82fd9c425
Alvaro Vargas Guerrero <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-07-25 13:14:47 +0200

    Performed namesPerIteration test on every variant implemented so far + changed function to work with output objects

--- graph/namesPerIteration.py
+++ graph/namesPerIteration.py
@@ -5,13 +5,17 @@ import MatrixFactory as mf
 import Strategy
 
 def namesPerIteration(ng, matrixNetwork):
-  plt.title(f"Average Names Invented in Total Per Iteration ({ng.name})")
+  plt.title(f"Average Names Invented in Total Per Iteration ({ng.name}, {ng.strategy.__name__})")
 
   plt.xlabel("Games played per agent")
 
   plt.ylabel("Amount of Names Invented")
 
-  yvalues = ng.start(matrixNetwork)
+  output = ng.start(matrixNetwork)
+
+  nameTable = output["names"]
+
+  yvalues = np.mean(nameTable, axis=1)
 
   plt.plot(yvalues)
 
@@ -21,11 +25,11 @@ factory = mf.MatrixFactory(triangular=True)
 
 lattice = factory.makeLatticeMatrix(40, 4)
 
-Baseline = BaselineNG(iterations=100, strategy=Strategy.mono)
+Baseline = BaselineNG(iterations=100, strategy=Strategy.mono, output=["names"])
 
-imi = Imitation(iterations=100, strategy=Strategy.mono)
+imi = Imitation(iterations=100, strategy=Strategy.mono, output=["names"])
 
-imi2 = Imitationv2(iterations=100, strategy=Strategy.mono)
+imi2 = Imitationv2(iterations=100, strategy=Strategy.mono, output=["names"])
 
 namesPerIteration(Baseline, lattice)
 
