249b6c094c621989f035718c6ce1b140786fde21
Alvaro Vargas Guerrero <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-07-25 14:21:32 +0200

    Moved ABNG to variants and added an adopt

--- variants/ABNG.py
+++ variants/ABNG.py
@@ -7,6 +7,5 @@ class ABNG(BaselineNG):
     if flipBit:
       name = 'A'
     else: name = 'B'
-    self.memory[agent].append((name, topic))
-    print("Agent " + str(agent) + " has invented the name " + name + " for topic " + str(topic))
+    self.adopt(name, topic, agent, agent)
     return name
