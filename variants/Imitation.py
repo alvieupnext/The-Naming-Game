4c6aefdcb8f5730215e36f0b13145108717eac6f
Alvaro Vargas Guerrero <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-07-25 13:31:01 +0200

    Refactored remove to removeTopic in ImitationGame and also moved displays and logic

--- variants/Imitation.py
+++ variants/Imitation.py
@@ -3,42 +3,40 @@ from variants.BaselineNG import *
 #remove procedure could be moved into its own class (if desired)
 class Imitation(BaselineNG):
   #Code for removing a topic from Agent memory
-  def remove(self, topic, agent):
+  def removeTopic(self, topic, agent):
     for pairName, pairTopic in self.memory[agent]:
       #if name of pair corresponds with the adopted name
       if pairTopic == topic:
         #remove pair from memory and circulation
-        self.memory[agent].remove((pairName, topic))
+        self.remove(pairName, topic, agent)
         self.circulation[pairName].remove(agent)
         # if circulation list for name empty, remove name key
         if not self.circulation[pairName]:
           self.circulation.pop(pairName)
-        self.display(f"Removed this pair from Agent memory {agent}: {pairName}, {pairTopic}")
 
   def adopt(self, name, topic, listener, speaker):
     #remove all previous bindings of the topic with the name
-    self.remove(topic, listener)
+    self.removeTopic(topic, listener)
     # if succesfull, run super
     super().adopt(name, topic, listener, speaker)
 
 #according to Guy Nagels, only success should clear memory
 class Imitationv2(BaselineNG):
   #Code for removing a topic from Agent memory, preserving the original binding
-  def remove(self, topic, agent, name):
+  def removeTopic(self, topic, agent, name):
     for pairName, pairTopic in self.memory[agent]:
       #if name of pair corresponds with the adopted name
       if pairTopic == topic and name != pairName:
         # remove pair from memory and circulation
-        self.memory[agent].remove((pairName, topic))
+        self.remove(pairName, topic, agent)
         self.circulation[pairName].remove(agent)
         #if circulation list for name empty
         if not self.circulation[pairName]:
           self.circulation.pop(pairName)
-        self.display(f"Removed this pair from Agent memory {agent}: {pairName}, {pairTopic}")
   #same code but only performs clear on success
   def success(self, speaker, listener, topic, name):
     #remove all previous bindings of the topic from both speaker and listener memory
-    self.remove(topic, speaker, name)
-    self.remove(topic, listener, name)
+    self.removeTopic(topic, speaker, name)
+    self.removeTopic(topic, listener, name)
     #invoke super
     super().success(speaker, listener, topic, name)
