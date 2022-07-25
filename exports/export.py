0df23430b90b720eed074b4da84d4538e66f30c7
Alvaro Vargas Guerrero <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-07-25 13:12:16 +0200

    Removed abstract code from functions (unnecessary) and added name attribute

--- exports/export.py
+++ exports/export.py
@@ -1,26 +1,29 @@
-from abc import ABC, abstractmethod
 #define the structure of an output objects, these objects will decide what output the naming game returns
-class output(ABC):
+class export:
+  def __init__(self, name):
+    #every export object knows its own name
+    self.name = name
+
   #setup to be performed before starting the game
-  @abstractmethod
   def setup(self, ng):
     pass
 
   #code to be performed on afterevery Iteration
-  @abstractmethod
-  def everyIteration(self, ng, sim, it):
+  def everyIteration(self, sim, it):
     pass
 
   #code to be performed after every simulation
-  @abstractmethod
-  def everySimulation(self, ng, sim):
+  def everySimulation(self, sim):
+    pass
+
+  #code to be performed after every invention
+  def everyInvent(self):
     pass
 
   #code to be performed after finishing the game
-  def teardown(self, ng):
+  def teardown(self):
     pass
 
   #what has to be returned after finishing the game
-  @abstractmethod
-  def output(self, ng):
+  def output(self):
     pass
