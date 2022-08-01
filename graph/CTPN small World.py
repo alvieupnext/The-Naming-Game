da9ec999071420e312aa8c71879d4b55c9e67a8d
Alvaro Vargas Guerrero <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-08-01 12:57:29 +0200

    Added Consensus Time per neighbour voor small world

---
+++ graph/CTPN small World.py
@@ -0,0 +1,94 @@
+from matplotlib import pyplot as plt
+from variants.BaselineNG import *
+from variants.Imitation import *
+from variants.ABNG import *
+import MatrixFactory as mf
+import Strategy
+import numpy as np
+from pylab import plot, show, \
+                  legend, boxplot, setp
+
+numberOfAgents = 100
+
+consensusScoreList = [0.8, 0.9,0.95, 0.98, 0.99, 1]
+
+ng = ABNG(maxIterations=2000, simulations=100, strategy=Strategy.multi, output=["popularity", "consensus"],
+          consensusScore=consensusScoreList, display=False)
+
+maxNeighbourSize = 40
+
+randomLinks = 6
+
+#test every single neighbourhood size from 2 to maximum
+neighboursizes = list(range(5, maxNeighbourSize + 1, 5))
+
+plt.title(f"Consensus Time Per Neighbourhood Size (Small World)({ng.name}, {ng.strategy.__name__}, {ng.simulations} simulations, {numberOfAgents} agents, random links {randomLinks})")
+
+plt.ylabel("Amount of Games played")
+
+plt.xlabel("Neighbour Size of Agent")
+
+colors = ['blue', 'red', 'cyan', 'magenta', 'green', 'purple']
+
+#help procedures for the boxplot
+# function for setting the colors of the box plots pairs
+def setBoxColors(bp):
+  for index, box in enumerate(bp['boxes']):
+    setp(box, color=colors[index])
+
+consensusIterations = []
+
+#create an empty matrix (rows are consensusScores, columns are number of neighbours)
+consensusMatrix = np.zeros((len(neighboursizes), len(consensusScoreList)), dtype=object)
+
+positions = [[n for n in range(i, i + len(consensusScoreList))] for i in range(0, len(neighboursizes) * len(consensusScoreList), len(consensusScoreList))]
+
+print(positions)
+
+ticks = [np.mean(lst) for lst in positions]
+print(ticks)
+# ax.set_xticks(ticks)
+
+for row, neighbour in enumerate(neighboursizes):
+  print(f"Using Neighbour Size {neighbour}")
+  lattice = mf.MatrixFactory().makeSmallWorldMatrix(numberOfAgents, neighbour, randomLinks)
+  output = ng.start(lattice)
+  #get list of when consensus was reached for every simulation
+  consensusList = output["consensus"]
+  reformattedConsensusList = []
+  for value in consensusScoreList:
+    reformattedConsensusList.append([])
+  for simulationConsensus in consensusList:
+    for index, set in enumerate(simulationConsensus):
+      #get the right iteration from consensusList and append it to the reformatted list
+      reformattedConsensusList[index].append(set[1])
+  print(reformattedConsensusList)
+  for column, values in enumerate(reformattedConsensusList):
+    consensusMatrix[row, column] = values
+
+lines = [plot([1,1], color = color)[0] for color in colors]
+
+consensusScoreStringList = [f"Convergence Rate : {rate}" for rate in consensusScoreList]
+
+legend(lines,consensusScoreStringList)
+
+list(map(lambda handle: handle.set_visible(False), lines))
+
+for index, row in enumerate(consensusMatrix):
+  bp = boxplot(row, positions=positions[index], widths=0.6)
+  setBoxColors(bp)
+  # ngroup = len(row)
+  # clevels = np.linspace(0., 1., ngroup)
+  # for position, simValues in zip(positions[index], row):
+  #   positionList = [position] * len(simValues)
+  #   print(positionList)
+  #   print(simValues)
+  #   plt.scatter(positionList, simValues, alpha=0.4)
+
+
+
+
+plt.xticks(ticks, neighboursizes)
+
+show()
+
