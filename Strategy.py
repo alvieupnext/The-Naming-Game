10c58b115e6e71bcbb1e7e7b81d01146ada131a2
Alvaro Vargas Guerrero <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-08-16 15:00:01 +0200

    Changed strategy code to turn the 3 float array produced from weighted agent pairs to a 2 int array with speaker and listener

--- Strategy.py
+++ Strategy.py
@@ -1,13 +1,21 @@
 import random
 #Various strategies for choosing which agent pairs become the speakers and listeners
+#Every strategy gets a list of 3-member list (speaker, listener, score) and returns a 2-member list of chosen agents
+
+def extractPair(agentPair):
+  speaker = agentPair[0]
+  listener = agentPair[1]
+  #reconvert agent pair to int
+  return [int(speaker), int(listener)]
 
 #Multi-learning strategy: every single agent pair learns something
 def multi(agentPairs):
-  return agentPairs
+  return list(map(extractPair, agentPairs))
 
 #Mono-learning: only one randomly chosen pair gets to learn
 def mono(agentPairs):
   index = random.randint(0, len(agentPairs) - 1)
   chosenPair = agentPairs[index]
   #Return pair in a list
-  return [chosenPair]
+  agentPairList = [chosenPair]
+  return list(map(extractPair, agentPairList))
