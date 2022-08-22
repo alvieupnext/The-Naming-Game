d1bf7983afdb11b21ef6d3d9301301b890482bd6
Alvaro Vargas Guerrero <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-08-22 14:42:24 +0200

    Reverted Only taking 10 names

---
+++ csv/simulationParallel.py
@@ -0,0 +1,89 @@
+# This code is responsible for testing parallel computing on the simulations
+import sys
+import os
+
+sys.path.append('C:/Users/alvar/PycharmProjects/MAP')
+
+import pandas as pd
+import numpy as np
+from multiprocessing import Pool
+from patientData import *
+import Strategy
+from variants.ABNG import *
+from functools import reduce
+
+consensusScoreList = [0.8, 0.9, 0.95, 0.98, 0.99, 1]
+
+scoresStringList = [f"SC_{score}" for score in consensusScoreList]
+
+columns = ['NG sim', 'subject']
+
+patientName = names[0]
+
+columns.extend(scoresStringList)
+
+groupSize = 1
+
+df = pd.DataFrame(columns=columns)
+
+totalSimulations = 10
+
+
+def getSimDataFromPatient(patient):
+  ng = ABNG(maxIterations=5000, simulations=1, strategy=Strategy.multi, output=["popularity", "consensus"],
+            consensusScore=consensusScoreList, display=False)
+  print(f"Using Patient Data {patient}")
+  data = readPatientData(patient)
+  output = ng.start(data)
+  consensusList = output["consensus"]
+  for sim, simValues in enumerate(consensusList):
+      # extract the convergence values from the simValues
+    reformattedSimValues = list(map(lambda set: set[1], simValues))
+      # if the array isn't the right size, fill rest of space with max iterations (not converged)
+    while len(reformattedSimValues) < len(consensusScoreList):
+        reformattedSimValues.append(ng.maxIterations)
+      # add simulation number and patient to an array
+    row = [sim, patient]
+      # extend it with the reformatted simulation values
+    row.extend(reformattedSimValues)
+      # add row to dataframe
+    df.loc[len(df.index)] = row
+  print(f"Finished using patient data {patient}")
+  return df
+
+
+def mergeData(sum, df):
+  return pd.merge(sum, df, how='outer')
+
+patientData = pd.DataFrame(columns=columns)
+
+
+
+if __name__ == "__main__":
+  with Pool(10) as pool:
+    dataFrames = pool.map(getDataFromPatient, patientGroups)
+    patientData = reduce(mergeData, dataFrames)
+    print(patientData)
+    patientData.to_csv("output/convergencePerPatient(N_back_Reduced)_weighted_2.csv")
+
+# if __name__ == "__main__":
+#   ray.init()
+#   try:
+#     patientDataRemotes = []
+#     for patientChunk in patientGroups:
+#       patientDataRemotes.append(getDataFromPatient.remote(patientChunk))
+#
+#     patientData = pd.DataFrame(columns=columns)
+#
+#     while len(patientDataRemotes):
+#       doneRemote, patientDataRemotes = ray.wait(patientDataRemotes, timeout=None)
+#       print("Finished one")
+#       patientData = mergeData(patientData, ray.get(doneRemote[0]))
+#
+#     patientData.to_csv("output/convergencePerPatient(N_back_Reduced)_weighted_2.csv")
+#     print(patientData)
+#   finally:
+#     ray.shutdown()
+
+
+
