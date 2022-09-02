aba06df46597fc64071867d04c5c876dccd1650c
Alvaro Vargas Guerrero <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-09-02 16:34:01 +0200

    Fixed bug with csv

--- ray_script.py
+++ ray_script.py
@@ -68,7 +68,7 @@ if __name__ == "__main__":
     doneRemote, patientDataRemotes = ray.wait(patientDataRemotes, timeout=None)
     print("Finished one")
     patientData = mergeData(patientData, ray.get(doneRemote[0]))
-    patientData.to_csv("output/convergencePerPatient(N_back_Reduced)_Ray.csv")
+    patientData.to_csv("csv/output/convergencePerPatient(N_back_Reduced)_Ray.csv")
 
 
 
