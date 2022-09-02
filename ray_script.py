6f22ba7bb720595741973216205161eef7d4eb68
Alvaro Vargas Guerrero <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-09-02 16:08:41 +0200

    Added a ray exec script for hydra

---
+++ ray_script.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+# This code is responsible for exporting NG convergence results to a csv file (using multicore)
+import sys
+import os
+
+import pandas as pd
+import numpy as np
+from multiprocessing import Pool
+from patientData import *
+import Strategy
+from variants.ABNG import *
+from functools import reduce
+import ray
+
+consensusScoreList = [0.8, 0.9, 0.95, 0.98, 0.99, 1]
+
+scoresStringList = [f"SC_{score}" for score in consensusScoreList]
+
+columns = ['NG sim', 'subject']
+
+patientNames = names
+
+columns.extend(scoresStringList)
+
+groupSize = 1
+
+patientGroups = [patientNames[i:i + groupSize] for i in range(0, len(patientNames), groupSize)]
+
+@ray.remote
+def getDataFromPatient(patientList):
+  ng = ABNG(maxIterations=100000, simulations=100, strategy=Strategy.multi, output=["popularity", "consensus"],
+            consensusScore=consensusScoreList, display=False)
+  df = pd.DataFrame(columns=columns, dtype=int)
+  for patient in patientList:
+    print(f"Using Patient Data {patient}")
+    data = readPatientData(patient)
+    output = ng.start(data)
+    consensusList = output["consensus"]
+    for sim, simValues in enumerate(consensusList):
+      # extract the convergence values from the simValues
+      reformattedSimValues = list(map(lambda set: set[1], simValues))
+      # if the array isn't the right size, fill rest of space with max iterations (not converged)
+      while len(reformattedSimValues) < len(consensusScoreList):
+        reformattedSimValues.append(ng.maxIterations)
+      # add simulation number and patient to an array
+      row = [sim, patient]
+      # extend it with the reformatted simulation values
+      row.extend(reformattedSimValues)
+      # add row to dataframe
+      df.loc[len(df.index)] = row
+    print(f"Finished using patient data {patient}")
+  return df
+
+
+def mergeData(sum, df):
+  return pd.merge(sum, df, how='outer')
+
+if __name__ == "__main__":
+  ray.init(address='auto')
+  patientDataRemotes = []
+  for patientChunk in patientGroups:
+    patientDataRemotes.append(getDataFromPatient.remote(patientChunk))
+
+  patientData = pd.DataFrame(columns=columns)
+
+  while len(patientDataRemotes):
+    doneRemote, patientDataRemotes = ray.wait(patientDataRemotes, timeout=None)
+    print("Finished one")
+    patientData = mergeData(patientData, ray.get(doneRemote[0]))
+    patientData.to_csv("output/convergencePerPatient(N_back_Reduced)_Ray.csv")
+
+
+
+
+
