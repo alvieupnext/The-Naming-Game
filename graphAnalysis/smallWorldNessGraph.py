a2d63bc4c10294fa19f0caef6b1f2ec299772d98
Alvaro Vargas Guerrero <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-09-09 17:37:24 +0200

    Implemented smallWorld

---
+++ graphAnalysis/smallWorldNessGraph.py
@@ -0,0 +1,26 @@
+import bct
+from patientData import *
+from smallWorldNess import *
+from matplotlib import pyplot as plt
+
+array = loadHPCData("netmats2")
+
+patientNames = array
+
+smallWorldNesses = []
+
+
+for patient in patientNames:
+  clustering_coef = bct.clustering_coef_wu(patient)
+  meanCluster = np.mean(clustering_coef)
+  characteristic_path_length = bct.charpath(patient)
+  smallWorld = smallWorldNess(meanCluster, characteristic_path_length[0])
+  smallWorldNesses.append(smallWorld)
+
+# plt.title("Character Path Lengths")
+plt.title("SmallWorldNess")
+
+
+plt.hist(smallWorldNesses)
+
+plt.show()
