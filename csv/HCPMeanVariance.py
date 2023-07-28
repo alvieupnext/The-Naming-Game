e94a9d581f05d5819d8708f4d056fe2b00f1eecd
alvieupnext <alvarovargasguerrero@hotmail.com>
2023-07-28 14:45:01 +0200

    Graph Circle Analysis

--- csv/HCPMeanVariance.py
+++ csv/HCPMeanVariance.py
@@ -13,16 +13,19 @@ def processDataFrame(df):
 
    # Compute the mean and variance for each SC column per subject
     mean_values = grouped_df.mean()
+    median_values = grouped_df.median()
     var_values = grouped_df.var()
 
    # Rename the columns to include the 'mean' and 'var' prefixes
+    median_columns = {col: f'{col}_median' for col in median_values.columns}
     mean_columns = {col: f'{col}_mean' for col in mean_values.columns}
     var_columns = {col: f'{col}_var' for col in var_values.columns}
     mean_values.rename(columns=mean_columns, inplace=True)
     var_values.rename(columns=var_columns, inplace=True)
+    median_values.rename(columns=median_columns, inplace=True)
 
     # Merge the mean and variance DataFrames into a single DataFrame
-    processed_df = pd.concat([mean_values, var_values], axis=1)
+    processed_df = pd.concat([mean_values, var_values, median_values], axis=1)
 
     # Assign the correct subject to the DataFrame
     processed_df['subject'] = processed_df.index
@@ -48,17 +51,29 @@ def processDataFrame(df):
 # Specify the file path to the CSV
 file_path = 'output/convergenceHCP_20percent.csv'
 
+file_path_2 = 'output/convergenceHCP_20percent_15_2ndpart.csv'
+
 # Read the CSV file into a DataFrame
 df = pd.read_csv(file_path, index_col="Unnamed: 0")
 
-print(df)
+df2 = pd.read_csv(file_path_2, index_col="Unnamed: 0")
+
+# print(df)
+#
+# print(df2)
+
+#Merge the DataFrames
+df = pd.concat([df, df2], axis=0)
+
+#Reset the index
+df.reset_index(inplace=True, drop=True)
 
 df = processDataFrame(df)
 
 print(df)
 
-# Assuming your DataFrame is named df
-output_file_path = 'output/convergenceHCP_20percent_mean_variance.csv'
+# # Assuming your DataFrame is named df
+output_file_path = 'output/convergenceHCP_20percent_mean_median_variance_25.csv'
 
 # Export the DataFrame to a CSV file
 df.to_csv(output_file_path, index=False)
--- graph/generated/preferredAction.py
+++ graph/generated/preferredAction.py
@@ -4,20 +4,26 @@ from variants.Imitation import *
 import MatrixFactory as mf
 from variants.ABNG import *
 import Strategy
+
+
+def map_choices(choices):
+  if len(choices) == 0:
+    return 0
+  elif len(choices) == 2:
+    return 3
+  elif choices[0] == 'A':
+    return 1
+  else:
+    return 2
+
+
 def preferredAction(ng, actionMatrix):
 
   fig, ax = plt.subplots()
   numberMatrix = np.zeros(actionMatrix.shape)
   for x in range(actionMatrix.shape[0]):
     for y in range(actionMatrix.shape[1]):
-      if len(actionMatrix[x,y]) == 0 :
-        #indecisive
-        numberMatrix[x,y] = 0
-      elif len(actionMatrix[x,y]) == 2:
-        numberMatrix[x,y] = 3
-      elif actionMatrix[x,y][0] == 'A':
-        numberMatrix[x, y] = 1
-      else: numberMatrix[x, y] = 2
+      numberMatrix[x,y] = map_choices(actionMatrix[x,y])
   ax.set_title(f"Preferred Action per Agent per Iteration ({ng.strategy.__name__})")
   ax.set_ylabel("Games played per agent")
   ax.set_xlabel("Agents")
@@ -37,9 +43,11 @@ scaleFree = factory.makeScaleFreeMatrix(100)
 
 smallWorld = factory.makeSmallWorldMatrix(100,5, 3)
 
-ab = ABNG(simulations=1, maxIterations=1000, strategy=Strategy.multi, output=["preferredAction"], consensusScore=[1], display=False)
+ab = ABNG(simulations=1, maxIterations=100, strategy=Strategy.multi, output=["preferredAction"], consensusScore=[1], display=False)
 
 output = ab.start(smallWorld)["preferredAction"][0]
+
+print(output)
 
 preferredAction(ab, output)
 
